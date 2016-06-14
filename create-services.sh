02f6f94ae12e157f771e569b36ff797ee7a8a93e
Craig Olrich <colrich@pivotal.io>
2016-06-14 14:55:11 -0400

    creation of services like SCS by the concourse build

--- create-services.sh
+++ create-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cf api $CF_API --skip-ssl-validation
-cf target -o $CF_ORG -s $CF_SPACE
 cf login -u $CF_USER -p $CF_PASS
+cf target -o $CF_ORG -s $CF_SPACE
 cf cs p-service-registry standard $JRC_EUREKA
